e5ab9120d0253cec6b8c2a6d241361df631a1ca9
TheDoniki <h18donme@du.se>
2023-02-13 07:10:45 +0100

    Correct react app w/ firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB28cdd2fLxVfk5M3ughQLx8A65S0DionE",
+  authDomain: "donikamehmeti-178e3.firebaseapp.com",
+  projectId: "donikamehmeti-178e3",
+  storageBucket: "donikamehmeti-178e3.appspot.com",
+  messagingSenderId: "512851820888",
+  appId: "1:512851820888:web:9c5300f50501d75ddb252d",
+  measurementId: "G-PCJQ83LMBS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// eslint-disable-next-line
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
